0a35df3cd25310efb9799eb751192d0068b00bb4
Jean-Pierre Bachmann <Jean-Pierre_Bachmann@live.de>
2020-11-30 13:53:22 +0100

    Init Add

---
+++ BlazoredBugNo3/BlazoredBugNo3/Pages/IndexBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blazored.Video;
+using Blazored.Video.Support;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazoredBugNo3.Pages
+{
+	public class IndexBase : ComponentBase
+	{
+		public IndexBase()
+		{
+			
+		}
+
+		public BlazoredVideo BlazoredVideo { get; set; }
+
+		public void OnPlayerPlayed(VideoState obj)
+		{
+			Console.WriteLine("State is " + (obj?.ToString() ?? "NULL"));
+		}
+	}
+}
